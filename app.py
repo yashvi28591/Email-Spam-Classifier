7feffa69c0119edc1689e0509b17ae0c110c19b8
yashvi28591 <yashvi28591@gmail.com>
2026-06-06 17:29:22 +0530

    Fix NLTK resources

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pickle
 import string
 import nltk
 
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
